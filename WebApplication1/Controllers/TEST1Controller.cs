9841d87e0fdaa2b812c4d59c36ef908d647d8b78
intimation14 <yours.thera@Gmail.com>
2016-10-15 23:03:04 +0800

    09 建立 Clients 控制器與相關頁面，改善 /Clients/Index 頁面的顯示

---
+++ WebApplication1/Controllers/TEST1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TEST1Controller : Controller
+    {
+        // GET: TEST1
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
